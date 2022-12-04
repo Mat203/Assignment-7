ac731ee8d327c7155ac77969acd9604beb9bdd11
Mat_Tol <matvey.talalaevskiy@gmail.com>
2022-12-04 16:57:09 +0200

    Basic program structure

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+ 
+n = sys.argv[1]
 
+with open('data.csv','r') as file:
+    next_line = file.readline()
+
+    while next_line:
+        next_line = file.readline()
+        print(next_line)
