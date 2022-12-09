f196eed1e1da6ab77b4755d708a356524baf6a4a
Mat_Tol <matvey.talalaevskiy@gmail.com>
2022-12-09 14:46:32 +0200

    Added argparse

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', type=argparse.FileType('r'))
+parser.add_argument('-medals', dest='medals', nargs=2, required=True)
+parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
 
 def medals(line, country, year,k):
     olympic_info = line.split('\t')
@@ -13,19 +18,18 @@ def medals(line, country, year,k):
         if olympic_info[14] == 'Bronze\n':
             k['Bronze'] += 1
     return k
+args = parser.parse_args()
 
-n = sys.argv
-print(n)
-with open('data.tsv','r') as file:
+with args.infile as file:
     next_line = file.readline()
     k = {'amount': 0, 'Gold': 0, 'Silver': 0, 'Bronze': 0, 'Name':''}
     while next_line:
-        if n[2] == '-medals':
+        if args.medals is not None:
             next_line = file.readline()
             if next_line != '' and k['amount'] < 10:
-                a = medals(next_line, n[3],n[4],k)
+                a = medals(next_line,args.medals[0], args.medals[1],k)
     print(k['Gold'], k['Silver'], k['Bronze'])
-    if n[5] == '-output':
+    if args.output is not None:
         f = open('result.txt','w')
         names = k['Name'].split(';')
         for name in names:
