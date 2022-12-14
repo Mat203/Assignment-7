35f4f077392e920c28ce83b5dd1a470a95bee4dd
Mat_Tol <matvey.talalaevskiy@gmail.com>
2022-12-14 11:54:24 +0200

    Basic task3 structure

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ parser.add_argument('infile', type=argparse.FileType('r'))
 parser.add_argument('-medals', dest='medals', nargs=2)
 parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
 
+
 def medals(line, country, year,sport_info):
     olympic_info = line.split('\t')
     a = olympic_info[6] == country or olympic_info[7] == country
@@ -19,23 +20,35 @@ def medals(line, country, year,sport_info):
         if olympic_info[14] == 'Bronze\n': sport_info['Bronze'] += 1
     return sport_info
 
+def overall(line, overall_dict):
+    olympic_info = line.split('\t')
+    for i in overall_dict:
+        if i == olympic_info[6] and olympic_info[14] != 'NA\n':
+            overall_dict[i] = overall_dict[i] + olympic_info[9] + ';'
+    return overall_dict
+
+
 args = parser.parse_args()
 
 with args.infile as file:
     next_line = file.readline()
     sport_info = {'amount': 0, 'Gold': 0, 'Silver': 0, 'Bronze': 0, 'Name_disc_medal':''}
-    while next_line:
+    overall_dict = dict.fromkeys(args.overall,'')
+
+    while next_line != '':
+        next_line = file.readline()
         if args.medals is not None:
-            next_line = file.readline()
-            if next_line != '' and sport_info['amount'] < 10:
+            if sport_info['amount'] < 10:
                 a = medals(next_line,args.medals[0], args.medals[1],sport_info)
+        if args.overall is not None:
+            next_line = file.readline()
+            if next_line !='': overall_dict = overall(next_line,overall_dict)
+    
     if args.medals is not None: print(sport_info['Gold'], sport_info['Silver'], sport_info['Bronze'])
     if args.output is not None:
-        f = open(args.output)
-        names = sport_info['Name'].split(';')
+        names = sport_info['Name_disc_medal'].split(';')
         for name in names:
-            f.write(name)
-        f.write(str(sport_info['Gold'])+' '+str(sport_info['Silver'])+' '+str(sport_info['Bronze']))
-        f.close()
+            args.output.writelines(name)
+        args.output.writelines(str(sport_info['Gold'])+' '+str(sport_info['Silver'])+' '+str(sport_info['Bronze']))
     if sport_info['amount'] == 0 and args.medals is not None:
         print('В обраний рік країна не брала участі в олімпіаді')      
